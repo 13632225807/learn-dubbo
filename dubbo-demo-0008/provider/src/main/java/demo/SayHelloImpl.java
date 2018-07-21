7dd6048208422112d8bee78599a4ad5733b58312
letian <sunlt1699@gmail.com>
2018-07-21 11:43:45 +0800

    add demo 0008

---
+++ dubbo-demo-0008/provider/src/main/java/demo/SayHelloImpl.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class SayHelloImpl implements SayHello {
+    public String sayHello(String name) {
+        return "Hello, " + name;
+    }
+}
---
+++ dubbo-demo-0008/spring-web/src/main/java/demo/springmvc/SpringWebApplication.java
@@ -0,0 +1,17 @@
+package demo.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(value = {
+        "classpath:dubbo-consumer.xml"
+})
+public class SpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebApplication.class, args);
+    }
+
+}
